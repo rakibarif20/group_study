9d66adc149ccaab86f8a27526a46e053707c639e
rakibarif20 <82168096+rakibarif20@users.noreply.github.com>
2023-11-26 23:16:47 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Index from "./Components/Project_1/Index";
+// import { AppProvider } from "./Components/Project_18/context";
+
+const App = () => {
+  return (
+    // <AppProvider>
+      <Index />
+    // </AppProvider>
+  );
+};
+
+export default App;
